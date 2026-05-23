f5a14172a196885fcc620c1b86c883f8b380c701
S. Jansen <samuelanton@hotmail.com>
2026-05-23 20:55:09 +0200

    implement solution

--- src/logic/maze/generator.ts
+++ src/logic/maze/generator.ts
@@ -25,71 +25,35 @@ export namespace Generator {
     resetMaze(cells);
 
     // First fill all the walls for the perimeter and the solution path
-    drawPerimeterWalls(cells);
+    // drawPerimeterWalls(cells);
     drawSolutionWalls(cells);
 
-    // The, find a random wall inside the perimeter (should be on the solution path if this is the first run
+    while (cells.some(row => row.some(cell => !cell.explored))) {
+      console.log("Finding new starting point")
 
-    // Then, open that wall and randomly create/walk a path over all unexplored cells until a random number hits or there's no valid path available
-
-    // Then start over by finding a new random wall anywhere inside the perimeter
-
+      // Then, find a random wall inside the perimeter (should be on the solution path if this is the first run)
+      let nextCell: Cell | null = getRandomStartCell(cells);
+      // Then, open that wall and randomly create/walk a path over all unexplored cells until a random number hits or there's no valid path available
+      while (nextCell && Math.random() > 0.03) {
+        nextCell = walkFromCell(cells, nextCell);
+      }
+      // Then start over by finding a new random wall anywhere inside the perimeter
+    }
   }
 
   const resetMaze = (cells: Cell[][]) => {
     cells.forEach(row => row.forEach(cell => {
-      cell.walls = [0, 0, 0, 0];
-      cell.explored = false;
+      cell.walls = [1, 1, 1, 1];
+      cell.explored = cell.isSolution;
     }))
-  }
-
-  const drawPerimeterWalls = (cells: Cell[][]) => {
-    cells.forEach((row, y) => {
-      row[0].walls[3] = 1;
-      row[row.length - 1].walls[1] = 1;
-
-      if (y == 0) {
-        row.forEach(cell => cell.walls[0] = 1);
-      }
-      if (y == cells.length - 1) {
-        row.forEach(cell => cell.walls[2] = 1);
-      }
-    })
   }
 
   const drawSolutionWalls = (cells: Cell[][]) => {
     const solution = cells.flatMap(row => row.filter(it => it.isSolution));
-    solution.forEach(cell => cell.walls = [1, 1, 1, 1]);
 
     solution.forEach(cell => {
-      const adjacent = solution
-        .filter(it => it != cell)
-        .filter(it => {
-          const diffX = Math.abs(it.x - cell.x);
-          const diffY = Math.abs(it.y - cell.y);
-          if (diffX > 1 || diffY > 1) return false; // Not a neighbor
-          if (diffX == diffY) return false; // Diagonal neighbor
-          return true;
-        });
-
-      // Remove walls between the cells
-      adjacent.forEach(it => {
-        const diffX = it.x - cell.x;
-        const diffY = it.y - cell.y;
-        if (diffX < 0) {
-          cell.walls[3] = 0;
-          it.walls[1] = 0;
-        } else if (diffX > 0) {
-          cell.walls[1] = 0;
-          it.walls[3] = 0;
-        } else if (diffY < 0) {
-          cell.walls[0] = 0;
-          it.walls[2] = 0;
-        } else if (diffY > 0) {
-          cell.walls[2] = 0;
-          it.walls[0] = 0;
-        }
-      })
+      const adjacent = getNeighbours([solution], cell);
+      adjacent.forEach(it => removeWallBetweenCells(cell, it));
 
       // Remove walls on outside
       if (adjacent.length == 1) {
@@ -99,5 +63,60 @@ export namespace Generator {
         if (cell.y == cells.length - 1) cell.walls[2] = 0;
       }
     })
+  }
+
+  const getRandomStartCell = (cells: Cell[][]): Cell => {
+    const explored = cells.flatMap(row => row.filter(it => it.explored));
+    if (explored.length == 0) {
+      // Get random cell
+      return cells[getRandomIndex(cells.length)][getRandomIndex(cells[0].length)];
+    }
+
+    return explored[getRandomIndex(explored.length)];
+  }
+
+  const getRandomIndex = (max: number) => Math.floor(Math.random() * max)
+
+  const getNeighbours = (cells: Cell[][], cell: Cell) =>
+    cells.flatMap(row => row)
+      .filter(it => it != cell)
+      .filter(it => {
+        const diffX = Math.abs(it.x - cell.x);
+        const diffY = Math.abs(it.y - cell.y);
+        if (diffX > 1 || diffY > 1) return false; // Not a neighbor
+        if (diffX == diffY) return false; // Diagonal neighbor
+        return true;
+      });
+
+  const removeWallBetweenCells = (a: Cell, b: Cell) => {
+    const diffX = b.x - a.x;
+    const diffY = b.y - a.y;
+    if (diffX < 0) {
+      a.walls[3] = 0;
+      b.walls[1] = 0;
+    } else if (diffX > 0) {
+      a.walls[1] = 0;
+      b.walls[3] = 0;
+    } else if (diffY < 0) {
+      a.walls[0] = 0;
+      b.walls[2] = 0;
+    } else if (diffY > 0) {
+      a.walls[2] = 0;
+      b.walls[0] = 0;
+    }
+  };
+
+  const walkFromCell = (cells: Cell[][], cell: Cell): Cell | null => {
+    // Get random unexplored neighbor cell
+    const neighbors = getNeighbours(cells, cell);
+    const unexplored = neighbors.filter(it => !it.explored);
+    if (unexplored.length == 0) return null;
+
+    const nextCell = unexplored[getRandomIndex(unexplored.length)];
+    nextCell.explored = true;
+
+    // Remove wall between the cells
+    removeWallBetweenCells(cell, nextCell);
+    return nextCell;
   }
 }
